d2ab33c1aff4d4e03daa40a76782b5b995250c13
Brad <bgommo.code@gmail.com>
2023-10-09 17:23:36 +0100

    Update password match checker with regex
    Password now required to contain upper/lowercase, number and special
    character.
    
    Still to add visual feedback for user.

--- scripts.js
+++ scripts.js
@@ -29,6 +29,10 @@ function sailHome() {
     const logo = document.getElementById('logo');
     logo.style.transform = 'translate(0px, 0px)';
     const checks = document.querySelectorAll('.checkMark');
+    const passwordOutline = document.querySelectorAll('.passwordInput');
+    passwordOutline.forEach((input) => {
+        input.classList.remove('match');
+    })
     checks.forEach((check) => {
         check.remove();
     })
@@ -54,24 +58,61 @@ const confirmPassword = document.getElementById("confirmpass");
 password.addEventListener("input", checkPassword);
 confirmPassword.addEventListener("input", checkPassword);
 
+let noCapCount = false;
+const noCap = document.createElement('span');
+
 function checkPassword() {
+    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&_*-]).{6,}$/;
+    if(password.value.match(passwordRegex) && password.nextElementSibling == null) {
+        const checkMark = document.createElement('span');
+        checkMark.classList.add("checkMark");
+        checkMark.innerText = "✓";
+        checkMark.style = "color:green; position:absolute; right:0px; top:17px;"
+        password.insertAdjacentElement('afterend', checkMark);
+    }
+
+    /*if (password.value.contains(/[A-Z]/)) {
+        console.log('do we have anything');
+        document.querySelector('.noCap').remove();
+    } else {
+        noCap.classList.add('noCap');
+        noCap.innerText = "Passwords must contain at least 1 Uppercase character."
+        form.appendChild(noCap);
+        noCapCount = true;
+    }
+
+    */
+
+
+    if(confirmPassword.value.match(passwordRegex) && confirmPassword.nextElementSibling == null && password.value == confirmPassword.value) {
+        const checkMark = document.createElement('span');
+        checkMark.classList.add("checkMark");
+        checkMark.innerText = "✓";
+        checkMark.style = "color:green; position:absolute; right:0px; top:17px;"
+        confirmPassword.insertAdjacentElement('afterend', checkMark);
+    } else {
+        if(confirmPassword && confirmPassword.nextElementSibling){
+            confirmPassword.parentNode.removeChild(confirmPassword.nextSibling);
+        }
+    }
 
     if (password.value != confirmPassword.value) {
         password.classList.add('error');
         confirmPassword.classList.add('error');
         password.classList.remove('match');
         confirmPassword.classList.remove('match');
-    } else {
+    }
+     else {
         password.classList.add('match');
         confirmPassword.classList.add('match');
         password.classList.remove('error');
         confirmPassword.classList.remove('error');
     }
+
 }
 
 /* validation check mark */
-
-const inputs = document.querySelectorAll('input');
+const inputs = document.querySelectorAll('.checkInputs');
 
 inputs.forEach((input) => {
     input.addEventListener('input', (e) => {
@@ -84,13 +125,9 @@ inputs.forEach((input) => {
         checkMark.innerText = "✓";
         checkMark.style = "color:green; position:absolute; right:0px; top:17px;"
         input.insertAdjacentElement('afterend', checkMark);
-        console.log("we got something");
     }else {
-        console.log("still something")
         if(input.nextSibling) {
             input.nextSibling.remove();
         }
     }})
 })
-
-
